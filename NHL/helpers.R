537b8ad862c3d90af4f0db4e5342e1f1d050ac3c
mscampb4 <mscampb4@ncsu.edu>
2025-07-04 10:39:31 -0400

    Add some useful data for input checks

--- NHL/helpers.R
+++ NHL/helpers.R
@@ -3,7 +3,12 @@ library(jsonlite)
 library(tidyverse)
 
 #---------- USEFUL DATA ---------- 
-
+#Valid abbreviations for API
+team_abbr <- c("ANA", "BOS", "BUF", "CAR", "CBJ", "CGY", "CHI", "COL", 
+               "DAL", "DET", "EDM", "FLA", "LAK", "MIN", "MTL", "NJD", 
+               "NSH", "NYI", "NYR", "OTT", "PHI", "PIT", "SEA", "SJS", 
+               "STL", "TBL", "TOR", "UTA", "VAN", "VGK", "WPG", "WSH", 
+               "ARI")
 
 #---------- HELPER FUNCTIONS ---------- 
 
