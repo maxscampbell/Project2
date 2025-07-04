9abcac04be1082c807d111bacd05b7a860ffd938
mscampb4 <mscampb4@ncsu.edu>
2025-07-04 10:22:18 -0400

    Landing page

--- NHL/app.R
+++ NHL/app.R
@@ -8,6 +8,7 @@
 #
 
 library(shiny)
+library(bslib)
 
 source("helpers.R")
 
@@ -20,7 +21,20 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     navset_pill(
       nav_panel("About",
-                #Content goes here
+                div("This is an R Shiny app designed to explore a 
+                  subset of NHL Data in an exploratory manner!"),
+                div("The data here is sourced from the official NHL API. While there is no official documentation, an unofficial reference can be found",
+                         a("here", href = "https://github.com/Zmalski/NHL-API-Reference")),
+                div("Thanks Zmalski!"),
+                h2("Usage"),
+                h3("Data Download"),
+                div("In the Data Download tab, you can view the data directly after it was pulled and cleaned from the API."),
+                div("You can also customize inputs, so you can view different teams, player positions, etc."),
+                div("You can even save the dataset that you create!"),
+                h3("Data Exploration"),
+                div("In this tab, you will be able to see visualizations of the data you have selected!"),
+                div("Included are scatterplots, contingency tables, etc."),
+                img(src = "nhl.png", align = "center")
                 ),
       nav_panel("Data Download",
                 #Content goes here
